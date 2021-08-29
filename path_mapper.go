daa42fe1255faefd7bfbbf37d6b2bfd431becec8
Zirou Kamikaze <kamikaze.zirou@gmail.com>
2021-08-30 01:46:13 +0900

    refactor

--- path_mapper.go
+++ path_mapper.go
@@ -10,41 +10,44 @@ import (
 // Mapping a URL or other path to a structure.
 //goland:noinspection GoUnusedExportedFunction
 func Mapping(pattern, path string, st interface{}) error {
-	pathSegments := strings.Split(path, "/")
 	patternSegments := strings.Split(pattern, "/")
+	pathSegments := strings.Split(path, "/")
 
 	if len(pathSegments) != len(patternSegments) {
 		return fmt.Errorf("pattern(%v) does not match path(%v)", pattern, path)
 	}
 
 	sv := reflect.ValueOf(st).Elem()
-	for i := 0; i < len(pathSegments); i++ {
-		pathSegment := pathSegments[i]
+	for i := 0; i < len(patternSegments); i++ {
 		patternSegment := patternSegments[i]
+		pathSegment := pathSegments[i]
 
 		if strings.HasPrefix(patternSegment, "{") && strings.HasSuffix(patternSegment, "}") {
-			n := patternSegment[1 : len(patternSegment)-1]
-			n = strings.Title(n)
-
-			f := sv.FieldByName(n)
-			switch f.Kind() {
-			case reflect.Int:
-				if v, err := strconv.ParseInt(pathSegment, 10, 0); err == nil {
-					f.SetInt(int64(v))
-				} else {
-					return fmt.Errorf(
-						"failed mapping %v to %v because %v is not int",
-						pathSegment,
-						f,
-						pathSegment,
-					)
-				}
-			case reflect.String:
-				f.SetString(pathSegment)
+			err := setField(sv, patternSegment[1:len(patternSegment)-1], pathSegment)
+			if err != nil {
+				return fmt.Errorf(": %w", err)
 			}
 		} else if pathSegment != patternSegment {
 			return fmt.Errorf("pattern(%v) does not match path(%v)", pattern, path)
 		}
 	}
+	return nil
+}
+
+func setField(sv reflect.Value, n, v string) error {
+	n = strings.Title(n)
+
+	f := sv.FieldByName(n)
+	switch f.Kind() {
+	case reflect.Int:
+		if v, err := strconv.ParseInt(v, 10, 0); err == nil {
+			f.SetInt(v)
+		} else {
+			return fmt.Errorf("failed mapping %v to %v because it is not int", v, f)
+		}
+	case reflect.String:
+		f.SetString(v)
+	}
+
 	return nil
 }
